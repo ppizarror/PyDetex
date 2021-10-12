371561728b98fb7d62ab095823b8acf5c64cd3a9
ppizarror <pablo@ppizarror.com>
2021-10-12 18:43:22 -0300

    Language detection

---
+++ test/test_utils.py
@@ -0,0 +1,26 @@
+"""
+PyDetex
+https://github.com/ppizarror/pydetex
+
+TEST UTILS
+Test utils.
+"""
+
+from test._base import BaseTest
+
+import pydetex.utils as ut
+
+
+class UtilsTest(BaseTest):
+
+    def test_lang(self) -> None:
+        """
+        Test language recognition.
+        """
+        s = """From anchor-based frameworks, Wu et al. [1] used Mask-RCNN [2] to vectorize the walls
+            by finding a rectangle proposal representing each segment's width, thickness, angle,
+            and location. """
+        self.assertEqual(ut.detect_language(s), 'en')
+        self.assertEqual(ut.get_language_tag('en'), 'English')
+        s = """El modelo propuesto contiene diferentes métricas para coordenar las tareas de segmentación"""
+        self.assertEqual(ut.detect_language(s), 'es')
