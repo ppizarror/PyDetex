3ac0381f9d53c740811c3dd583ef738905ededcb
ppizarror <pablo@ppizarror.com>
2021-10-18 16:32:15 -0300

    Added dictionary ico to hook

--- pydetex/__pyinstaller/hook-pydetex.py
+++ pydetex/__pyinstaller/hook-pydetex.py
@@ -44,6 +44,7 @@ def _append_to_datas(file_path: str, target_folder: str, base_target_folder: str
 # Append resources
 res_files = [
     'res/cog.ico',
+    'res/dictionary.ico',
     'res/icon.gif',
     'res/icon.ico',
     'res/stopwords.json'
