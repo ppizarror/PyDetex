c122d7b89b9d835bb76d884bafa43ad828ad2637
ppizarror <pablo@ppizarror.com>
2022-02-13 19:18:35 -0300

    Add py39 to setup

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python',
         'Topic :: Multimedia',
         'Topic :: Text Processing'
