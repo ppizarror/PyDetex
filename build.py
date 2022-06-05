23ecf5cf0a544c6321482c17374727b738bfc9b7
ppizarror <pablo@ppizarror.com>
2022-06-06 00:16:48 +0100

    Fix python path

--- build.py
+++ build.py
@@ -11,7 +11,6 @@ import sys
 
 assert len(sys.argv) == 2, 'Argument is required, usage: build.py pyinstaller/pip/twine/gource'
 mode = sys.argv[1].strip()
-python = 'python3' if not sys.platform == 'win32' else 'py -3'
 sys_arch = struct.calcsize('P') * 8
 
 if mode == 'pyinstaller':
@@ -19,7 +18,7 @@ if mode == 'pyinstaller':
     upx = ''
     if sys.platform == 'win32' and sys_arch == 64:
         upx = '--upx-dir build/upx_64'
-    pyinstaller = f'{python} -m PyInstaller' if sys.platform == 'win32' else 'pyinstaller'
+    pyinstaller = f'python -m PyInstaller' if sys.platform == 'win32' else 'pyinstaller'
 
     # os.system(f'{pyinstaller} specs/PyDetex_Win.spec --noconfirm {upx}')
     os.system(f'{pyinstaller} specs/PyDetex_Win_Single.spec --noconfirm {upx}')
@@ -38,11 +37,11 @@ elif mode == 'pip':
         for k in os.listdir('build'):
             if 'bdist.' in k or k == 'lib':
                 os.system(f'rm -rf build/{k}')
-    os.system(f'{python} setup.py sdist --dist-dir dist/pip bdist_wheel --dist-dir dist/pip')
+    os.system(f'python setup.py sdist --dist-dir dist/pip bdist_wheel --dist-dir dist/pip')
 
 elif mode == 'twine':
     if os.path.isdir('dist/pip'):
-        os.system(f'{python} -m twine upload dist/pip/*')
+        os.system(f'python -m twine upload dist/pip/*')
     else:
         raise FileNotFoundError('Not distribution been found, execute build.py pip')
 
