2cdebf48a141c0303b1549d084795f783e73d613
ppizarror <pablo@ppizarror.com>
2025-03-28 19:57:58 -0300

    Fix invalid description for pypi release

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ import pydetex
 
 # Load readme
 with open('README.rst', encoding='utf-8') as f:
-    long_description = f.read()
+    long_description: str = f.read()
+    long_description = long_description.split('Install Instructions')[0].strip()
 
 # Load requirements
 with open('requirements.txt', encoding='utf-8') as f:
